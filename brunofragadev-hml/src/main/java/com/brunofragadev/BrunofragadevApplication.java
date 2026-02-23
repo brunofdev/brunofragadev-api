a1595444912092bcbcb7f5c6e43790eb0f6983bf
Bruno Fraga <brunofragaa97@gmail.com>
2026-02-23 11:34:54 -0300

    ajustado e configurado ambiente de desenvolvimento

--- brunofragadev-hml/src/main/java/com/brunofragadev/BrunofragadevApplication.java
+++ brunofragadev-hml/src/main/java/com/brunofragadev/BrunofragadevApplication.java
@@ -8,6 +8,8 @@ public class BrunofragadevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrunofragadevApplication.class, args);
+        System.out.println("Servidor inicializado com sucesso!");
 	}
+
 
 }
