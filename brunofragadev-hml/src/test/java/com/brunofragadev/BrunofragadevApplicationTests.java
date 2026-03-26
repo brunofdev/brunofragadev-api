7dc762ccca9e2b2ac105130afd6a0893abdd3c74
Bruno Fraga <brunofragaa97@gmail.com>
2026-03-26 15:38:03 -0300

    desativado teste integrado

--- brunofragadev-hml/src/test/java/com/brunofragadev/BrunofragadevApplicationTests.java
+++ brunofragadev-hml/src/test/java/com/brunofragadev/BrunofragadevApplicationTests.java
@@ -1,9 +1,11 @@
 package com.brunofragadev;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Desativado porque exige banco de dados real no CI")
 class BrunofragadevApplicationTests {
 
 	@Test
